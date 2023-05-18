de9500f0f891b00758829b9dae3d080ce4fd0fc0
Ovidiu Lipianu <ovidexter4@gmail.com>
2023-05-18 17:20:00 +0300

    First version of the Mini audio editor app: User can: -upload audio file -play/pause/unpause song -adjust audio volume Better looks

--- main.py
+++ main.py
@@ -8,29 +8,67 @@ class AudioPlayerApp(tk.Tk):
         super().__init__()
         self.title("Audio Player")
         self.geometry("400x200")
+        self.configure(bg="#323232")  # Set background color
 
         self.file_path = ""
         self.is_playing = False
         self.is_paused = False
 
-        self.file_label = tk.Label(self, text="No file selected")
-        self.file_label.pack(pady=10)
+        self.file_label = tk.Label(
+            self, text="No file selected", bg="#323232", fg="#ffffff", font=("Arial", 12)
+        )
+        self.file_label.pack(pady=20)
 
-        self.open_button = tk.Button(self, text="Open", command=self.open_file)
+        self.open_button = tk.Button(
+            self,
+            text="Open",
+            command=self.open_file,
+            bg="#4a4a4a",
+            fg="#ffffff",
+            font=("Arial", 12),
+            padx=20,
+            pady=10,
+        )
         self.open_button.pack()
 
-        self.play_button = tk.Button(self, text="Play", command=self.play_pause_song, state=tk.DISABLED)
+        self.play_button = tk.Button(
+            self,
+            text="Play",
+            command=self.play_pause_song,
+            state=tk.DISABLED,
+            bg="#4a4a4a",
+            fg="#ffffff",
+            font=("Arial", 12),
+            padx=20,
+            pady=10,
+        )
         self.play_button.pack()
 
-        self.volume_label = tk.Label(self, text="Volume")
+        self.volume_label = tk.Label(
+            self, text="Volume", bg="#323232", fg="#ffffff", font=("Arial", 12)
+        )
         self.volume_label.pack()
 
-        self.volume_slider = tk.Scale(self, from_=0, to=100, orient=tk.HORIZONTAL, command=self.adjust_volume)
-        self.volume_slider.set(100);
+        self.volume_slider = tk.Scale(
+            self,
+            from_=0,
+            to=100,
+            orient=tk.HORIZONTAL,
+            command=self.adjust_volume,
+            bg="#323232",
+            fg="#ffffff",
+            sliderlength=20,
+            showvalue=False,
+            length=300,
+            highlightthickness=0,
+        )
+        self.volume_slider.set(100)
         self.volume_slider.pack()
 
     def open_file(self):
-        self.file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.mp3;*.wav")])
+        self.file_path = filedialog.askopenfilename(
+            filetypes=[("Audio Files", "*.mp3;*.wav")]
+        )
         if self.file_path:
             self.file_label.config(text="Selected file: " + self.file_path)
             self.play_button.config(state=tk.NORMAL)
